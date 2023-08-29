2def7907d5c418a002e48186fc44dffef19111b6
Lucila Costa <lucilagdac@gmail.com>
2023-08-29 14:09:34 -0300

    up

--- UNI1/q6.c
+++ UNI1/q6.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 int main(void) {
-    int pulo[];
-    
+    // a questão quer qual expressão referencia o terceiro elemento do vetor, que é o 2
+    int pulo[4] = {0, 1, 2, 3};
+    printf("%d\n", *(pulo+2)); // Saída: 2
+    printf("%d\n", *(pulo+3)); // Saída: 3
+    printf("%d\n", pulo+3); // Saída: 6422300
+    printf("%d\n", pulo+2); // Saída: 6422296
 }
---
+++ UNI1/q7.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    int mat[4], *p, x;
+    // p = mat + 1;
+    // p = mat++;
+    // p = ++mat;
+    // x = (*mat)++;
+    printf(p);
+}
